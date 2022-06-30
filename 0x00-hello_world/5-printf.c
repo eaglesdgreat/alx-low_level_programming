efa750bc70dbabb17f25d88e11ca307b7858e954
Emmanuel <okponobiemmanuel@gmail.com>
2022-06-30 16:20:57 -0700

    Update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Description: prints "with proper grammar, but the outcome is a piece of art,\n"*
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
