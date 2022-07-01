619e048bc492a49cd6a294c11eb97ae3575b77d2
Emmanuel <okponobiemmanuel@gmail.com>
2022-06-30 17:04:44 -0700

    update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: prints "A funtion that print without printf and puts"*
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char stri[] = "and that piece of art is useful\" \
+		       - Dora Korpar, 2015-10-19\n";
+
+	write(2, stri, 59);
+	return (1);
+}
