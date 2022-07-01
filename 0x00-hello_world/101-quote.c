a9f5363c9914e25c002cf4bc07ab62102bdd62ec
Emmanuel <okponobiemmanuel@gmail.com>
2022-06-30 17:09:10 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 
 int main(void)
 {
-	char stri[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char stri[] = "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n";
 
 	write(2, stri, 59);
 	return (1);
