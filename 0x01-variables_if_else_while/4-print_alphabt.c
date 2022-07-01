b5f4111583a65dc42d1db397d5214f2734f8679e
Emmanuel <okponobiemmanuel@gmail.com>
2022-07-01 14:11:01 -0700

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'q' && x != 'e')
+			putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
