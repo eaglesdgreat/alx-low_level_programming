2714953e6d57c8496f37b2da31c65d5379de35cd
Emmanuel <okponobiemmanuel@gmail.com>
2022-06-30 23:53:46 -0700

    changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 
 /**
  * main - Entry point
- * Description: prints "A funtion that print without printf and puts"*
- * Return: Always 0 (Success)
+ *
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	char stri[] = "and that piece of art is useful\" \
-- Dora Korpar, 2015-10-19\n";
+	char stri[] = "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n";
 
-	write(1, stri, 59);
+	write(2, stri, 59);
 	return (1);
 }
