9b15e0e5cb6ffbb4307c6a6cc96960a29068fdda
Emmanuel <okponobiemmanuel@gmail.com>
2022-07-05 17:25:21 -0700

    update

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always void
+*/
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
