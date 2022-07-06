f7a92df6bfe45323436ae367c8ba65f8f06f6206
Emmanuel <okponobiemmanuel@gmail.com>
2022-07-05 17:28:03 -0700

    changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -9,15 +8,15 @@
 
 int main(void)
 {
-	putchar(95);
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
+	_putchar(95);
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
